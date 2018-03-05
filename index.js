5c7969a6dfd55216646d62d9046fbb423423aa0a
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-03-05 17:53:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+var bot = new Discord.Client();
+
+const PREFIX = "!";
+
+
+bot.on("ready", function() {
+    bot.user.setGame('grupa.rl-polska.pl')
+    console.log("Im Ready!");
+});
+
+bot.on('messageReactionAdd', (reaction, user) => {
+            
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(PREFIX)) return;
+        var args = message.content.substring(PREFIX.length).split(" ");
+
+        switch (args[0].toLowerCase()) {
+            case "grupa":
+                message.channel.send("<http://grupa.rl-polska.pl");
+                break;
+            case "pomoc":
+                message.channel.send("***Aktualnie dostępne komendy:***\n\n**!grupa**\n**!administracja**");
+                break;
+            case "fanpage":
+                message.channel.send("<https://www.facebook.com/RLPolska/>");
+        }
+    }  
+)
+
+
+
+
+bot.login(process.env.BOT_TOKEN);
